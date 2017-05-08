5a0f733b89d3c807a8e3efe80dba70f132a6391c
Agnė Klimaitė <agnaite@me.com>
2017-05-08 10:46:48 -0700

    added checkout functionality and order history

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ class Store(object):
     """A store"""
 
     def add_user(self, email):
-        """Add user to the database"""
+        """Add user to the database."""
 
         user = User(email=email)
         db.session.add(user)
         db.session.commit()
 
         print "User added."
-
 
     def add_product(self, title, price, available_inventory):
         """Add product to the database"""
@@ -30,25 +29,54 @@ class Store(object):
 
         print "Product added."
 
+    def view_products(self):
+        """Print all the products in inventory."""
+
+        products = Product.query.all()
+
+        for product in products:
+            if product.available_inventory > 0:
+                print "[{}] {} ({}): ${:4,.2f}.".format(product.product_id,
+                                                        product.title,
+                                                        product.available_inventory,
+                                                        product.price)
+
     def view_cart(self, user):
-        """List products in user's cart"""
+        """List products in user's cart."""
+
+        cart = Cart.query.filter_by(user_id=user.user_id, complete=False).first()
+        total = 0
+
+        if cart:
+            for item in cart.cart_products:
+
+                product = Product.query.get(item.product_id)
+                subtotal = product.price * item.quantity
+                total += subtotal
+
+                print "[{}] {} ({}): ${:4,.2f}.".format(product.product_id,
+                                                        product.title,
+                                                        item.quantity,
+                                                        subtotal)
+        else:
+            print "Your cart is empty."
+
+        print "Total: ${:4,.2f}".format(total)
+
+    def get_cart(self, user):
+        """Return list of items in a user's cart."""
 
         cart = Cart.query.filter_by(user_id=user.user_id, complete=False).first()
 
         if cart:
-            cart_items = CartProduct.query.filter_by(cart_id=cart.cart_id).all()
-            for item in cart_items:
-                print "{} ({}): {}.".format(Product.query.get(item.product_id).title,
-                                            item.quantity,
-                                            Product.query.get(item.product_id).price)
-        else:
-            print "Your cart is empty."
+            return cart.cart_products
 
-    def add_product_to_cart(self, user, product, quantity):
-        """Add product to cart"""
+    def add_to_cart(self, user, product, quantity):
+        """Add product to cart."""
 
         cart = Cart.query.filter_by(user_id=user.user_id, complete=False).first()
 
+        # if user does not have an active cart, make a cart
         if not cart:
             cart = Cart(user_id=user.user_id,
                         cart_created=datetime.now(),
@@ -57,12 +85,101 @@ class Store(object):
             db.session.add(cart)
             db.session.commit()
 
-        cart_product = CartProduct(product_id=product.product_id,
-                                   cart_id=cart.cart_id,
-                                   quantity=quantity)
+        # if product's inventory does not exceed the quantity adding to cart,
+        # create an instance of cart_product
+        if product.available_inventory >= quantity:
 
-        db.session.add(cart_product)
-        db.session.commit()
+            cart_product = CartProduct(product_id=product.product_id,
+                                       cart_id=cart.cart_id,
+                                       quantity=quantity)
+
+            db.session.add(cart_product)
+            db.session.commit()
+            print "Product added."
+        else:
+            print "Out of stock."
+
+    def remove_from_cart(self, user, product):
+        """Remove product from cart."""
+
+        # get the items in user's cart
+        user_cart = Cart.query.filter_by(user_id=user.user_id, complete=False).first()
+
+        if user_cart:
+            # get the product to be removed from user's cart
+            product_to_remove = CartProduct.query.filter_by(cart_id=user_cart.cart_id,
+                                                            product_id=product.product_id).first()
+            if product_to_remove:
+                db.session.delete(product_to_remove)
+                db.session.commit()
+                print "Product removed from cart."
+            else:
+                print "Product not found."
+        else:
+            print "Your cart is empty."
+
+    def update_quantity_in_cart(self, user, product, new_quantity):
+        """Update the quantity of a product in user's cart."""
+
+        user_cart = Cart.query.filter_by(user_id=user.user_id, complete=False).first()
+        if user_cart:
+            # get the product to be updated from user's cart
+            product_to_update = CartProduct.query.filter_by(cart_id=user_cart.cart_id,
+                                                            product_id=product.product_id).first()
+            if product_to_update and product_to_update.product.available_inventory >= new_quantity:
+                product_to_update.quantity = new_quantity
+                db.session.commit()
+                print "Product quantity updated."
+            else:
+                print "Product not found or quantity requested exceeds product's inventory."
+
+    def checkout_cart(self, user):
+        """Checkout cart_products and mark cart as complete."""
+
+        user_cart = Cart.query.filter_by(user_id=user.user_id, complete=False).first()
+
+        # if there is an active carts that contains products,
+        # iterate over each product in active cart,
+        # check that inventory is still greater than quantity in cart,
+        # and update product's available inventory
+        if user_cart and user_cart.cart_products:
+            for product in user_cart.cart_products:
+                if product.quantity <= product.product.available_inventory:
+                    product.product.available_inventory -= product.quantity
+                else:
+                    print "{} is out of stock. Sorry!".format(product.product.title)
+
+            # mark cart as complete and add checkout timestamp
+            user_cart.complete = True
+            user_cart.cart_completed = datetime.now()
+            db.session.commit()
+            print "Thank you for shopping."
+        else:
+            print "Your cart is empty."
+
+    def view_purchase_history(self, user):
+        """Display all completed orders for a user."""
+
+        completed_orders = Cart.query.filter_by(user_id=user.user_id, complete=True).all()
+
+        for completed_order in completed_orders:
+            print '-' * 36
+            print "Order ID: {}".format(completed_order.cart_id)
+            print "Order date: {}".format(completed_order.cart_completed.strftime("%x %X"))
+
+            for product in completed_order.cart_products:
+                print "[{}] {}: ${:4,.2f}".format(product.product_id,
+                                                  product.product.title,
+                                                  product.product.price * product.quantity)
+
+        if not completed_orders:
+            print "No orders found for {}.".format(user.email)
+
+    def get_purchase_history(self, user):
+        """Return list of completed orders."""
+
+        completed_orders = Cart.query.filter_by(user_id=user.user_id, complete=True).all()
+        return completed_orders
 
 
 if __name__ == "__main__":
@@ -71,8 +188,7 @@ if __name__ == "__main__":
 
     # create an instance of store
     store = Store()
-
+    # grab user with id 1
     user = User.query.get(1)
-    store.add_product("The Pointed Slide", 145, 100)
 
     print "Use 'store' to invoke methods."
--- seed.py
+++ seed.py
@@ -1,12 +1,15 @@
-from model import User, connect_to_db, db
+from model import User, Product, connect_to_db, db
 from app import app
+
 
 def load_users():
     """Load users from user_mock_data into database."""
 
     print "Users"
 
-    for i, row in enumerate(open("data/MOCK_USER_DATA.csv")):
+    User.query.delete()
+
+    for i, row in enumerate(open("data/mock_user_data.csv")):
         row = row.rstrip()
         user_id, email, password = row.split(",")
 
@@ -20,8 +23,32 @@ def load_users():
 
     db.session.commit()
 
+
+def load_products():
+    """Load products from product_mock_data into database."""
+
+    print "Products"
+
+    # Product.query.delete()
+
+    for i, row in enumerate(open("data/mock_product_data.csv")):
+        row = row.rstrip()
+        title, price, inventory = row.split(",")
+
+        product = Product(title=title,
+                          price=price,
+                          available_inventory=inventory)
+
+        db.session.add(product)
+
+        # provide some sense of progress
+        if i % 100 == 0:
+            print i
+
+    db.session.commit()
+
 if __name__ == "__main__":
     connect_to_db(app)
 
-    load_users()
-    # set_val_user_id()
+    # load_users()
+    load_products()
